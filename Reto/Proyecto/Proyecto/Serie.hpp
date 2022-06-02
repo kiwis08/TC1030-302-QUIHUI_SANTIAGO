a7ee30c68a5f574a2e6ff3578e1603b587d94d24
Santiago Quihui <santiagoquihui@hotmail.com>
2022-06-02 16:26:38 -0500

    Añadir clase Serie

---
+++ Reto/Proyecto/Proyecto/Serie.hpp
@@ -0,0 +1,44 @@
+//
+//  Serie.hpp
+//  Proyecto
+//
+//  Created by Santiago Quihui on 02/06/22.
+//
+
+#ifndef Serie_hpp
+#define Serie_hpp
+
+
+#include <stdio.h>
+#include <iostream>
+#include "Video.hpp"
+#include "Episodio.hpp"
+
+using namespace std;
+
+class Serie: public Video {
+public:
+    // Constructores
+    Serie ();
+    Serie(string _id, string _titulo, int _duracion, string _genero, double _calificacionPromedio);
+    
+    // Setters
+    void setEpisodio(int _index, Episodio _episodio);
+    void setCantidad (int _cantidad);
+    
+    // Getters
+    Episodio getEpisodio (int _index);
+    int getCantidad ();
+    
+    // Otros Metodos
+    double calculaCalPromedio ();
+    string str();
+    
+private:
+    Episodio episodios[5];
+    int cantidad;
+    
+};
+
+
+#endif // Serie_hpp
