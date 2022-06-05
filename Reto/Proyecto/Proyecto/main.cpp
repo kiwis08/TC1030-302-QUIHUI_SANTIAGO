282cfd8bc88dfde28a02735fc938022739f9202c
Santiago Quihui <santiagoquihui@hotmail.com>
2022-06-04 19:41:57 -0500

    Actualizar main.cpp con el asignado por mastra

--- Reto/Proyecto/Proyecto/main.cpp
+++ Reto/Proyecto/Proyecto/main.cpp
@@ -1,64 +1,72 @@
 //
 //  main.cpp
-//  Proyecto
+//  Avance 2 Objetivos - Lectura de archivo, arreglo de apuntadores, operador new,  generación de reportes, Herencia.
 //
-//  Created by Santiago Quihui on 02/06/22.
+//  Created by Ma. Guadalupe Roque on 5/21/22.
+//  Copyright © 2022 Invitado. All rights reserved.
 //
+
 
 #include <iostream>
-
-#include "Video.hpp"
-#include "Pelicula.hpp"
-#include "Serie.hpp"
 #include "Peliculas.hpp"
-
-int main(int argc, const char * argv[]) {
-    // Declaracion de un video Default:
-    Video v1;
-    // Declaracion de un video con parametros:
-    Video v2("001", "Spider-Man: No Way Home", 120, "Acción", 10.0);
-    // Delcaracion de una pelicula Defualt:
-    Pelicula p1;
-    // Declaracion de una pelicula con parametros:
-    Pelicula p2("002", "Doctor Strange", 120, "Acción", 10.0, 1);
-    // Declaracion de una serie Defualt:
-    Serie s1;
-    // Declaracion de una serie con parametros:
-    Serie s2("003", "Modern Family", 30, "Comedia", 9.5);
-    // Creacion de episodio por Default:
-    Episodio ep1;
-    // Creacion de episodio con parametros:
-    Episodio ep2("The Bicycle Thief",1, 9.7);
-    
-    Peliculas coleccionPelis;
-    
-    cout << "---- VIDEOS -----" << endl;
-    cout << v1.str() << endl;
-    cout << v2.str() << endl;
-    
-    cout << "---- PELICULAS -----" << endl;
-    cout << p1.str() << endl;
-    cout << p2.str() << endl;
-    
-    cout << "---- EPISODIOS -----" << endl;
-    cout << ep1.str() << endl;
-    cout << ep2.str() << endl;
-    
-    
-    cout << "---- SERIES -----" << endl;
-    cout << s1.str();
-    
-    // Agregar episodios a la serie
-    s2.setCantidad(s2.getCantidad() + 1);
-    s2.setEpisodio(0,ep1);
-    s2.setCantidad(s2.getCantidad() + 1);
-    s2.setEpisodio(1,ep2);
-    
-    cout <<  s2.str() << endl;
-    
-    cout << "---- PELICULAS2 -----" << endl;
-    coleccionPelis.leerArchivo();
-    
-    return 0;
-    
+int menuPeliculas(){
+    int iOpcion;
+    /*
+    cout <<
+    "\n ** Peliculas " <<
+    "\n1. Leer Peliculas  desde Archivo" <<
+    "\n2. Reporte de todas las peliculas" <<
+    "\n3. Reporte de peliculas que tienen cierta Calificacion" <<
+    "\n4. Reporte de peliculas de cierto genero" <<
+    "\n0. SALIR\nTeclea la opcion:";
+    */
+    cin >> iOpcion;
+    return iOpcion;
 }
+// ******************************
+//            AVANCE 2
+// *******************************
+int main() {
+    // se manda llamar al constructor - default
+    Peliculas peliculas{};
+    
+    int iOpcion;
+    string sGenero, sId;
+    double dCal;
+  
+    // Leer el archivo de peliculas y cargarlo en el arreglo de apuntadores
+    // dentro de la clase Peliculas
+    peliculas.leerArchivo();
+    
+    // 1o Inicializar la vcc antes del ciclo
+    iOpcion = menuPeliculas();
+    // 2a Incluir en la condicion la vcc
+    while (iOpcion != 0){
+        switch (iOpcion) {
+                // ***** Peliculas
+            case 1: // 1. Leer Peliculas  desde Archivo" <<
+                peliculas.leerArchivo();
+                break;
+            case 2:// 2. Reporte de todas las peliculas" <<
+                peliculas.reporteTodasLasPeliculas();
+                break;
+            case 3:// 3. Reporte de todas las peliculas con cierta Calificacion" <<
+                //cout << "Ingresa la calificacion:";
+                cin >> dCal;
+                peliculas.reporteConCalificacion(dCal);
+                break;
+            case 4: // 4. Reporte de todas las peliculas con cierto genero" <<
+                //cout << "Ingresa el Genero:";
+                cin >> sGenero;
+                peliculas.reporteGenero(sGenero);
+                break;
+            default:
+                //cout << "Opcion Incorrecta!!\n";
+                break;
+        }
+        //3o Actualizar la vcc dentro del ciclo
+        iOpcion = menuPeliculas();
+    }
+    return 0;
+}
+
